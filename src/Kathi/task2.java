c7849f863bda6bf80a16af7d9c81df2b9728c267
Scheyk <kathrin.dostal@gmail.com>
2020-10-15 11:28:05 +0200

    finish task 2

---
+++ src/Kathi/task2.java
@@ -0,0 +1,13 @@
+package Kathi;
+
+public class task2 {
+
+    public static void main(String[]args){
+        float one = 45.2f;
+        int two = 78;
+        System.out.print(one +" ");
+        System.out.print(two +" ");
+        System.out.print("schey");
+
+    }
+}
