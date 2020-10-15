528c87d3e30f759a5568383cc26df7cbf3d07d23
Cornel Gurin <cornelgurin@gmail.com>
2020-10-15 12:00:59 +0200

    commit

--- src/Cornel/Task_02.java
+++ src/Cornel/Task_02.java
@@ -4,8 +4,8 @@ public class Task_02 {
 
     public static void main (String [] arg){
 
-        float x = (float) 45.2;
-        int y = 78;
+        double x =45.2;
+        double y = 78;
         String name = " Cornel";
 
         System.out.print(x+y);
---
+++ src/Cornel/Task_03.java
@@ -0,0 +1,18 @@
+package Cornel;
+
+public class Task_03 {
+
+
+    public static void main (String [] arg){
+
+        double x = 123;
+        double y = 592.14;
+        double sum = x + y ;
+
+
+        System.out.print("The sum of 123 and 529.14 is " + sum);
+
+
+    }
+
+}
