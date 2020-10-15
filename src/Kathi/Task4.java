e856ad568e74908ddda7a7cce86c1b67520624bf
Scheyk <kathrin.dostal@gmail.com>
2020-10-15 11:39:17 +0200

    finish task 4

---
+++ src/Kathi/Task4.java
@@ -0,0 +1,16 @@
+package Kathi;
+
+public class Task4 {
+
+    public static void main(String[]args){
+        int one = 125;
+        int two = 24;
+
+        System.out.println(one + " + " + two + " = " + (one + two));
+        System.out.println(one + " - " + two + " = " + (one - two));
+        System.out.println(one + " x " + two + " = " + (one * two));
+        System.out.println(one + " / " + two + " = " + (one / two));
+        System.out.println(one + " mod " + two + " = " + (one % two));
+
+    }
+}
