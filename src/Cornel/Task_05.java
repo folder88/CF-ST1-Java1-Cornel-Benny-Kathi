332b9f0a29fa2b1f30ceef780bd2f54f6ded173b
Cornel Gurin <cornelgurin@gmail.com>
2020-10-15 12:25:39 +0200

    commit

---
+++ src/Cornel/Task_05.java
@@ -0,0 +1,13 @@
+package Cornel;
+
+public class Task_05 {
+
+    public static void main (String [] arg){
+
+
+        System.out.println("   J    a  v     v  a");
+        System.out.println("   J   a a  v   v  a a");
+        System.out.println("J  J  aaaaa  V V  aaaaa");
+        System.out.println(" JJ  a     a  V  a     a");
+    }
+}
