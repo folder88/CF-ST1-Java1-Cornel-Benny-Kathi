0d5eb003bc6b8f1eee35f319bd1e6096c1b595d8
Cornel Gurin <cornelgurin@gmail.com>
2020-10-15 13:21:03 +0200

    commit

---
+++ src/Cornel/Task_06.java
@@ -0,0 +1,18 @@
+package Cornel;
+
+public class Task_06 {
+    public static void main (String [] arg){
+
+    System.out.println("  +\"\"\"\"\"+ ");
+    System.out.println(" [| ° ° |]");
+    System.out.println("  |  ^  | ");
+    System.out.println("  | '_' | ");
+    System.out.println("  +_____+ ");
+
+
+
+
+
+
+    }
+}
