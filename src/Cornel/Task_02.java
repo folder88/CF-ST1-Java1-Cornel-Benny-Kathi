23b6ff6f13e9ca0ac45e2ba0e8f30c9440d5087f
Cornel Gurin <cornelgurin@gmail.com>
2020-10-15 11:49:49 +0200

    commit

--- src/Cornel/Task_02.java
+++ src/Cornel/Task_02.java
@@ -4,8 +4,12 @@ public class Task_02 {
 
     public static void main (String [] arg){
 
+        float x = (float) 45.2;
+        int y = 78;
+        String name = " Cornel";
 
-        System.out.println("Hallo World");
-        System.out.println("Hallo World");
+        System.out.print(x+y);
+        System.out.print(name);
+
     }
 }
