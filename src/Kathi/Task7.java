47687bc91e71fe253a28c14b50b4ff863c00656e
Scheyk <kathrin.dostal@gmail.com>
2020-10-15 13:15:11 +0200

    finish task 7

---
+++ src/Kathi/Task7.java
@@ -0,0 +1,23 @@
+package Kathi;
+
+public class Task7 {
+
+    public static void main(String[]args){
+        int one = 12;
+        int two = 95;
+        int three = 3;
+        int four = 10;
+        float five = 45.7f;
+
+        float result = (one + two + three + four + five) / 5;
+
+        System.out.println("The average value of " + one +", " +two + ", " + three+ ", " + four + ", " + five + " is: "+ result);
+        //for(int i = 0; i < one.length; i++){
+        //    total += one[i];
+        //}
+
+        //double average = total / one.length;
+
+        //System.out.println("The average value of " + one[1] +", " +one[2] + ", " + one[3]+ ", " + one[4]+ ", " + one[5] + " is: "+ average);
+    }
+}
