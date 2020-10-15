739d56c718c5a459850d02b643df6f968b14309e
Scheyk <kathrin.dostal@gmail.com>
2020-10-15 11:33:50 +0200

    finish task 3

---
+++ src/Kathi/Task3.java
@@ -0,0 +1,13 @@
+package Kathi;
+
+public class Task3 {
+
+    public static void main(String[]args){
+        int one = 123;
+        float two = 592.14f;
+        float three = one + two;
+
+        System.out.println("The sum of " + one + " and " + two + " is: " + (one + two));
+        System.out.println("The sum of " + one + " and " + two + " is: " + three);
+    }
+}
