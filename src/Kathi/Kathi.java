19ebe4e6d207b0e41de8a45925e54cd72e9b6603
Scheyk <kathrin.dostal@gmail.com>
2020-10-15 11:02:16 +0200

    finish task 1

--- src/Kathi/Kathi.java
+++ src/Kathi/Kathi.java
@@ -2,9 +2,35 @@ package Kathi;
 
 public class Kathi {
 
-    public static void main (String [] arg){
+    public static void main(String[]args){
+        //a
+        int one = -5;
+        int two = 8;
+        int three = 6;
 
+        System.out.println(one + two * three);
 
-        System.out.println("Hallo World");
+        //b
+        int four = 55;
+        int five = 9;
+
+        System.out.println(four % five);
+
+        //c
+        int six = 20;
+        int seven = -3;
+        int aight = 5;
+        int nine = 8;
+        System.out.println(six + (seven * aight) / nine);
+
+        //d
+        int ten = 5;
+        int eleven = 15;
+        int twelve = 3;
+        int thirten = 2;
+        int fourten = 8;
+        int fiften = 3;
+        System.out.println(ten + eleven / twelve * thirten - fourten % fiften);
+
     }
 }
